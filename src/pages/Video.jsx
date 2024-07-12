e72409e693f7047ff4565a528bdac3ba9e2ba342
Shreyas sk <shreyassk.dev@gmail.com>
2024-07-12 15:37:50 +0530

    Project initialisation: UI creation

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,8 +1,24 @@
-
+import styles from "../styles/Video.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function Video() {
     return(
         <>
-            <h1>Video</h1>
+            <div className={styles.main}>
+                <header><h1>Atta</h1></header>
+                <div className={styles.content}>
+                    <div className={styles.video}>
+                        <div className={styles.videoSmall}></div>
+                    </div>
+                    <div className={styles.chat}>
+                        <div className={styles.msg}></div>
+                        <div className={styles.textArea}>
+                            <textarea placeholder="Type a message"></textarea>
+                            <button>send</button>
+                        </div>
+                    </div>
+                </div>
+                <footer><button>Skip</button></footer>
+            </div>
         </>
     )
 }
