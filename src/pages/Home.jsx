b7167b749a4d9c2992efcbb08eff53dcc28a7aba
Shreyas sk <shreyassk.dev@gmail.com>
2024-08-15 15:19:59 +0530

    feature: Add home page design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,40 @@
+import Logo from "../assets/drawing.svg";
+import styles from "../styles/Home.module.css";
+import { useNavigate } from "react-router";
 
 function Home() {
-    return(
-        <>
-            <h1>Home</h1>
-        </>
-    )
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className={styles.header}>
+        <h3>EnthaMone.com</h3>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.innerContainer}>
+          <span>ENGAGE WITH NEW PEOPLE</span>
+          <h1>
+            Connect, Chat, & Explore
+            <br />
+            Online
+          </h1>
+          <p>
+            Explore countless conversations with individuals from all over
+            <br />
+            Connect with New People Instantly
+          </p>
+          <button
+            onClick={() => {
+              navigate("/video");
+            }}
+          >
+            Talk to Strangers
+          </button>
+        </div>
+        <img className={styles.illustration} src={Logo} />
+      </div>
+    </>
+  );
 }
 
 export default Home;
