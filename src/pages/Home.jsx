10363fd0586ac24d04a1e4957a06ec031e583ecc
Shreyas sk <shreyassk.dev@gmail.com>
2024-09-14 16:12:35 +0530

    feat: Age Verification dialog added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,22 @@
 import Logo from "../assets/drawing.svg";
 import styles from "../styles/Home.module.css";
 import { useNavigate } from "react-router";
+import { useState, useRef } from "react";
 
 function Home() {
   const navigate = useNavigate();
+  const dialogContainer = useRef();
 
   return (
     <>
+      <dialog ref={dialogContainer} onClick={(e) => dialogContainer.current.close()}>
+        <h2>Age Verification</h2>
+        You must be 18 years or older to use this service.
+        <span>
+          <button className={styles.secondaryButton} onClick={() => {navigate("/chat")}}>I am 18 years or older</button>
+          <button className={styles.primaryButton} onClick={() => {window.location.href = 'https://google.com'}}>I am not 18</button>
+        </span>
+      </dialog> 
       <div className={styles.header}>
         <h3>Entha Mone</h3>
       </div>
@@ -25,7 +35,11 @@ function Home() {
           </p>
           <button
             onClick={() => {
-              navigate("/chat");
+              if (import.meta.env.VITE_AGE_CONFIRM === "true"){
+                dialogContainer.current.showModal();
+              } else {
+                navigate("/chat");
+              }
             }}
           >
             Talk to Strangers
